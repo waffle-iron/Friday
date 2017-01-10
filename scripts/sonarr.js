58bcb84a706877bade650f0b3893413564e611f2
Iain Brown <codeiain@outlook.com>
2017-01-10 13:04:25 +0000

    started building sonarr add show

--- scripts/sonarr.js
+++ scripts/sonarr.js
@@ -30,7 +30,7 @@ module.exports = function (robot) {
         var slackMsg = {
             "attachments": [
                 {
-                    "fallback":"List of all shows",
+                    "fallback": "List of all shows",
                     "pretext": "Shows",
                     "fields": [],
                 }
@@ -66,14 +66,14 @@ module.exports = function (robot) {
             var slackMsg = {
                 "attachments": [
                     {
-                        "fallback":"List of shows",
+                        "fallback": "List of shows",
                         "pretext": "Shows",
                         "fields": [],
                     }
                 ]
             };
-            for (var i = 0; i < data.length; i++){
-                 slackMsg.attachments[0].fields.push({
+            for (var i = 0; i < data.length; i++) {
+                slackMsg.attachments[0].fields.push({
                     "value": data[i].title,
                     "short": true
                 });
@@ -81,6 +81,56 @@ module.exports = function (robot) {
             }
             msg.send(slackMsg);
         });
+    });
+
+    robot.respond(/add show (.*)/i, function (msg) {
+        //api/series/lookup?term=The%20Blacklist
+        /*tvdbId:showData[0].tvdbId,
+        title:showData[0]:1,
+        titleSlug:showData[0].titleSlug,
+        images:showData[0].images,
+        seasons:showData[0].seasons
+
+        'Authorization': 'Basic '
+        */
+        var toFind = encodeURIComponent(msg.match[1]);
+        var url = "http://codeiain.myftp.org:8989/api/series/lookup?term=" + toFind + "&apikey=1c622d8bf0d64ddea5ae8562670894aa"
+        robot.http(url).get()(function (err, res, body) {
+            var showData = JSON.parse(body);
+            //console.log(showData.length);
+            if (showData.length == 1) {
+                var postdata = {
+                    tvdbId: showData[0].tvdbId,
+                    title: showData[0].title,
+                    qualityProfileId: 1,
+                    titleSlug: showData[0].titleSlug,
+                    images: showData[0].images,
+                    seasons: showData[0].seasons,
+                    rootFolderPath: "/home/pi/fileserv/Shared Videos/TV Shows/",
+                    apikey:'1c622d8bf0d64ddea5ae8562670894aa'
+                }
+                console.log(JSON.stringify(postdata));
+                robot.http("http://codeiain.myftp.org:8989/api/series/")
+                    //.header('Authorization', 'Basic 1c622d8bf0d64ddea5ae8562670894aa')
+                    .header('X-Api-Key', '1c622d8bf0d64ddea5ae8562670894aa')
+                    .post(postdata)(function (err, res, body) {
+                        console.log('err= '+err);
+                        console.log('res = ' +res);
+                        console.log('body= ' +body);
+
+                    });
+
+            }
+            else if (showData > 1) {
+
+            }
+            else {
+                msg.send("Can't find that  show");
+            }
+
+
+
+        })
     });
 
     function today() {
@@ -117,3 +167,16 @@ module.exports = function (robot) {
 
     }
 };
+
+
+
+/*
+
+data = JSON.stringify({
+  foo: 'bar'
+});
+
+robot = robot.http("https://midnight-train").header('Content-Type', 'application/json').post(data)(function(err, res, body) {});
+
+// ---
+*/
