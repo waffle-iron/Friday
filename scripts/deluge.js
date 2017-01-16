41258ca0b42c390113de46ac0b05813bb5afaa3a
Iain Brown <Iain.Brown@gbgplc.com>
2017-01-16 13:38:13 +0000

    fixed status issue

--- scripts/deluge.js
+++ scripts/deluge.js
@@ -18,11 +18,11 @@ module.exports = function (robot) {
     robot.respond(/deluge ?(status)?(.*)/i, function (msg) {
 
         var status = msg.match[1];
-        if (status != undefined && status != null){
-            if (status != "Active" && status != "Allocating" && status != "Checking" && status != "Downloading" && status != "Seeding" && status != "Paused" && status != "Error" && status != "Queued"){
-                    msg.send("Sorry thats not a valid status");
+        if (status != undefined && status != null) {
+            if (status != "Active" || status != "Allocating" || status != "Checking" || status != "Downloading" || status != "Seeding" || status != "Paused" || status != "Error" || status != "Queued") {
+                msg.send("Sorry thats not a valid status");
             }
-        }else{
+        } else {
             status = "Downloading";
         }
 
@@ -30,37 +30,41 @@ module.exports = function (robot) {
         var deluge = require('node-deluge')(process.env.DELUGE_WEB_URL, process.env.DELUGE_PASSWORD, null);
         deluge.get_status(function (data) {
             var torrents = data;
-            var slackMsg = {
-                "attachments": [
-                    {
-                        "fallback":"List of all downloading torrents",
-                        "pretext": "Current Torrents",
-                        "fields": [],
+            if (data) {
+                var slackMsg = {
+                    "attachments": [
+                        {
+                            "fallback": "List of all downloading torrents",
+                            "pretext": "Current Torrents",
+                            "fields": [],
+                        }
+                    ]
+                };
+                var length = Object.keys(torrents.result.torrents).length;
+                for (var key in torrents.result.torrents) {
+                    var obj = torrents.result.torrents[key];
+                    if (obj["state"] == status) {
+                        slackMsg.attachments[0].fields.push({
+                            "title": "Name",
+                            "value": obj["name"],
+                            "short": true
+                        });
+                        slackMsg.attachments[0].fields.push({
+                            "title": "Progress",
+                            "value": obj["progress"].toFixed(2) + "%",
+                            "short": true
+                        });
+                        slackMsg.attachments[0].fields.push({
+                            "title": "ETA",
+                            "value": obj['eta'],
+                            "short": true
+                        })
                     }
-                ]
-            };
-            var length = Object.keys(torrents.result.torrents).length;
-            for (var key in torrents.result.torrents) {
-                var obj = torrents.result.torrents[key];
-                if (obj["state"] == status) {
-                    slackMsg.attachments[0].fields.push({
-                        "title": "Name",
-                        "value": obj["name"],
-                        "short": true
-                    });
-                    slackMsg.attachments[0].fields.push({
-                        "title": "Progress",
-                        "value": obj["progress"].toFixed(2) + "%",
-                        "short": true
-                    });
-                    slackMsg.attachments[0].fields.push({
-                        "title":"ETA",
-                        "value":obj['eta'],
-                        "short":true
-                    })
                 }
+                msg.send(slackMsg);
+            } else {
+                msg.send("No torrents in that status");
             }
-            msg.send(slackMsg);
         });
     });
 
